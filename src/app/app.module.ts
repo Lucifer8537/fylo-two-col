ebd177fbd245b74cd45c677cca431635b340ccf4
navodit <navodit50@gmail.com>
2023-12-26 06:48:16 +0530

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { EmailComponent } from './email/email.component';
+import { FormsModule } from '@angular/forms';
+import { ProductiveComponent } from './productive/productive.component';
+import { EarlyAccessComponent } from './early-access/early-access.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, HeaderComponent, EmailComponent, ProductiveComponent, EarlyAccessComponent, FooterComponent],
+  imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
